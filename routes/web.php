4966ef5cc37bbc0ff02444b01864436b83d49a8c
Nambi Sembilu <nambisembilu@gmail.com>
2019-03-10 23:08:52 +0700

    Editing Routes, Add PDF Vendor and main.js

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,17 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 /* Master Resources */
 Route::resources([
     'class' => 'ClassController',
     'teacher' => 'TeacherController',
     'student' => 'StudentController',
 ]);
+
+/* Confiure Class */
+Route::get('/home', 'ConfigureController@index')->name('home');
+Route::get('configure/export_pdf', 'ConfigureController@export_pdf')->name('configure.export_pdf');
+Route::any('configure/teacher/{class_id}','ConfigureController@teacher')->name('configure.teacher');
+Route::any('configure/students/{class_id}','ConfigureController@student')->name('configure.student');
+Route::patch('configure/students_reset/{class_id}','ConfigureController@students_reset')->name('configure.students_reset');
 
